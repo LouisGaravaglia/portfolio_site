ef70166eba4c2a49693ecd8bc1b97abca9d5af74
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-05-29 15:33:11 -0700

    moved portfolioItems to its own file

--- src/PortfolioContainer.js
+++ src/PortfolioContainer.js
@@ -4,12 +4,7 @@ import './App.css';
 import PortfolioItem from "./PortfolioItem";
 import useViewport from "./hooks/useViewport"
 import NextArrow from './NextArrow';
-import ACOUSTIC_GIF from "./images/ACOUSTIC.mp4";
-import LYRCS_GIF from "./images/LYRCS.mp4";
-import WINENOT_GIF from "./images/WINENOT.mp4";
-import SMARTREACH_GIF from "./images/SMARTREACH.mp4";
-import PORTFOLIOSITE_GIF from "./images/PORTFOLIOSITE.mp4";
-import BRIGHTMOMENTS_GIF from "./images/BRIGHTMOMENTS.mp4";
+import portfolioItems from './portfolioItems';
 
 function Portfolio() {
   const [projectHover, setProjectHover] = useState(false);
@@ -32,69 +27,6 @@ function Portfolio() {
     gifWidth = '100vw';
   }
 
-  const portfolioItems = [
-    {
-      id: 1,
-      title: "Acoustigram.io",
-      gif: ACOUSTIC_GIF,
-      link: "http://www.acoustigram.io/",
-      githubLink: "https://github.com/LouisGaravaglia/test-acoustic-frontend",
-      summary: "***CURRENTLY IN DEVELOPMENT*** Acoustigram.io is a web app that will use AI to create playlists of new music releases it thinks you may like. Tech Stack: Typescript | React | Python | Django | TensorFlow | PostgreSQL",
-      mobileSummary: "Acoustigram.io is my latest side-project. A web app which aims to use AI to create playlists of new music it thinks you may like. Tech Stack: Typescript | React | Python | Django | TensorFlow | PostgreSQL",
-      classname: "LYRCS-Opacity"
-    },
-    {
-      id: 2,
-      title: "Bright Moments",
-      gif: BRIGHTMOMENTS_GIF,
-      link: "https://brightmoments.io/",
-      githubLink: "https://github.com/Bright-Moments",
-      summary: "Bright Moments is a physical NFT gallery in Venice, CA. I am the Frontend Engineer, turning Figma mockups into pixel-perfect implementation using React + Gatsby. Tech Stack: React | Gatsby | Node | Express",
-      mobileSummary: "Bright Moments is a physical NFT gallery in Venice, CA. I am the Frontend Engineer, turning Figma mockups into pixel-perfect implementation using React + Gatsby. Tech Stack: React | Gatsby | Node | Express",
-      classname: "LYRCS-Opacity"
-    },
-    {
-      id: 3,
-      title: "LYRCS",
-      gif: LYRCS_GIF,
-      link: "http://findlyrcs.herokuapp.com/",
-      githubLink: "https://github.com/LouisGaravaglia/iTranslate",
-      summary: "LYRCS is a web app that displays lyrics for a song in its original language as well as a translated language of the user's choice. Tech Stack: Javascript | React | Redux | Node | Express | PostgreSQL",
-      mobileSummary: "LYRCS is a web app that displays lyrics for a song in its original language as well as a translated language of the user's choice. Tech Stack: Javascript | React | Redux | Node | Express | PostgreSQL",
-      classname: "LYRCS-Opacity"
-    },
-    {
-      id: 4,
-      title: "The Smart Reach",
-      gif: SMARTREACH_GIF,
-      link: "https://www.thesmartreach.org/",
-      githubLink: "https://github.com/jamiesonreinhard/bridger-front-end",
-      summary: "The Smart Reach is a website for a non-profit that matches at-risk youth with volunteer tutors. Founding engineering team member. Tech Stack: React | SASS | Ruby on Rails",
-      mobileSummary: "The Smart Reach is a website for a non-profit that matches at-risk youth with volunteer tutors. Founding engineering team member. Tech Stack: React | SASS | Ruby on Rails",
-      classname: "LYRCS-Opacity"
-    },
-    {
-      id: 5,
-      title: "Wine-not!",
-      gif: WINENOT_GIF,
-      link: "https://findwinenot.herokuapp.com/",
-      githubLink: "https://github.com/LouisGaravaglia/Wine-not",
-      summary: "Would you like something new to drink? Wine-Not! A web app that allows users to find new Wines to enjoy. Tech Stack: Javascript | Python | Flask | PostgreSQL | Bulma",
-      mobileSummary: "Would you like something new to drink? Wine-Not! A web app that allows users to find new Wines to enjoy. Tech Stack: Javascript | Python | Flask | PostgreSQL | Bulma",
-      classname: "Wine-Not-Opacity"
-    },
-    {
-      id: 6,
-      title: "Portfolio site",
-      gif: PORTFOLIOSITE_GIF,
-      link: "https://louis-garavaglia.netlify.app/",
-      githubLink: "https://github.com/LouisGaravaglia/portfolio_site",
-      summary: "A clean and interactive site that puts the focus on the individual projects. Designed with a mobile-first approach. Tech Stack: HTML5 | CSS3 | React",
-      mobileSummary: "My portfolio site. A clean and minamalistic site that puts the focus on the individual projects. Designed with a mobile-first approach. Tech Stack: HTML5 | CSS3 | React",
-      classname: "Wine-Not-Opacity"
-    }
-  ];
-
   useEffect(() => {
     if (viewportWidth <= 700 && viewportHeight <= 650) setMobileMode(true);
     if (viewportWidth > 700 || viewportHeight > 650) setMobileMode(false);
@@ -115,7 +47,7 @@ function Portfolio() {
     mainBackground = "none"
   } else {
     mainBackground = "linear-gradient(to right, #63a198 0%, #8874c2 100%)";
-  }
+  };
 
   let gifBackground;
 
@@ -126,7 +58,7 @@ function Portfolio() {
         <source src={portfolioItems[resultsIdx].gif} type="video/mp4" />
       </video>
     </>
-  )
+  );
 
 ////////////////////////////////////////////////////  RETURN  ////////////////////////////////////////////////////
 
---
+++ src/portfolioItems.js
@@ -0,0 +1,71 @@
+import ACOUSTIC_GIF from "./images/ACOUSTIC.mp4";
+import LYRCS_GIF from "./images/LYRCS.mp4";
+import WINENOT_GIF from "./images/WINENOT.mp4";
+import SMARTREACH_GIF from "./images/SMARTREACH.mp4";
+import PORTFOLIOSITE_GIF from "./images/PORTFOLIOSITE.mp4";
+import BRIGHTMOMENTS_GIF from "./images/BRIGHTMOMENTS.mp4";  
+
+const portfolioItems = [
+  {
+    id: 1,
+    title: "Acoustigram.io",
+    gif: ACOUSTIC_GIF,
+    link: "http://www.acoustigram.io/",
+    githubLink: "https://github.com/LouisGaravaglia/test-acoustic-frontend",
+    summary: "***CURRENTLY IN DEVELOPMENT*** Acoustigram.io is a web app that will use AI to create playlists of new music releases it thinks you may like. Tech Stack: Typescript | React | Python | Django | TensorFlow | PostgreSQL",
+    mobileSummary: "Acoustigram.io is my latest side-project. A web app which aims to use AI to create playlists of new music it thinks you may like. Tech Stack: Typescript | React | Python | Django | TensorFlow | PostgreSQL",
+    classname: "LYRCS-Opacity"
+  },
+  {
+    id: 2,
+    title: "Bright Moments",
+    gif: BRIGHTMOMENTS_GIF,
+    link: "https://brightmoments.io/",
+    githubLink: "https://github.com/Bright-Moments",
+    summary: "Bright Moments is a physical NFT gallery in Venice, CA. I am the Frontend Engineer, turning Figma mockups into pixel-perfect implementation using React + Gatsby. Tech Stack: React | Gatsby | Node | Express",
+    mobileSummary: "Bright Moments is a physical NFT gallery in Venice, CA. I am the Frontend Engineer, turning Figma mockups into pixel-perfect implementation using React + Gatsby. Tech Stack: React | Gatsby | Node | Express",
+    classname: "LYRCS-Opacity"
+  },
+  {
+    id: 3,
+    title: "LYRCS",
+    gif: LYRCS_GIF,
+    link: "http://findlyrcs.herokuapp.com/",
+    githubLink: "https://github.com/LouisGaravaglia/iTranslate",
+    summary: "LYRCS is a web app that displays lyrics for a song in its original language as well as a translated language of the user's choice. Tech Stack: Javascript | React | Redux | Node | Express | PostgreSQL",
+    mobileSummary: "LYRCS is a web app that displays lyrics for a song in its original language as well as a translated language of the user's choice. Tech Stack: Javascript | React | Redux | Node | Express | PostgreSQL",
+    classname: "LYRCS-Opacity"
+  },
+  {
+    id: 4,
+    title: "The Smart Reach",
+    gif: SMARTREACH_GIF,
+    link: "https://www.thesmartreach.org/",
+    githubLink: "https://github.com/jamiesonreinhard/bridger-front-end",
+    summary: "The Smart Reach is a website for a non-profit that matches at-risk youth with volunteer tutors. Founding engineering team member. Tech Stack: React | SASS | Ruby on Rails",
+    mobileSummary: "The Smart Reach is a website for a non-profit that matches at-risk youth with volunteer tutors. Founding engineering team member. Tech Stack: React | SASS | Ruby on Rails",
+    classname: "LYRCS-Opacity"
+  },
+  {
+    id: 5,
+    title: "Wine-not!",
+    gif: WINENOT_GIF,
+    link: "https://findwinenot.herokuapp.com/",
+    githubLink: "https://github.com/LouisGaravaglia/Wine-not",
+    summary: "Would you like something new to drink? Wine-Not! A web app that allows users to find new Wines to enjoy. Tech Stack: Javascript | Python | Flask | PostgreSQL | Bulma",
+    mobileSummary: "Would you like something new to drink? Wine-Not! A web app that allows users to find new Wines to enjoy. Tech Stack: Javascript | Python | Flask | PostgreSQL | Bulma",
+    classname: "Wine-Not-Opacity"
+  },
+  {
+    id: 6,
+    title: "Portfolio site",
+    gif: PORTFOLIOSITE_GIF,
+    link: "https://louis-garavaglia.netlify.app/",
+    githubLink: "https://github.com/LouisGaravaglia/portfolio_site",
+    summary: "A clean and interactive site that puts the focus on the individual projects. Designed with a mobile-first approach. Tech Stack: HTML5 | CSS3 | React",
+    mobileSummary: "My portfolio site. A clean and minamalistic site that puts the focus on the individual projects. Designed with a mobile-first approach. Tech Stack: HTML5 | CSS3 | React",
+    classname: "Wine-Not-Opacity"
+  }
+];
+
+export default portfolioItems;
